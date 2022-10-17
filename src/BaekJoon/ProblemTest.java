5e1909b53d74cd730fd9ed2a2f884aa5deadc339
DESKTOP-FK4LHFN\prixi <prixia@naver.com>
2022-10-17 23:26:38 +0900

    단위테스트 환경 설정

---
+++ src/BaekJoon/ProblemTest.java
@@ -0,0 +1,17 @@
+package BaekJoon;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+class ProblemTest {
+
+	@Test
+	void test() throws IOException {
+		Problem19538 solve = new Problem19538();
+		solve.solution();
+	}
+
+}
