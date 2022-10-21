e680f307b3e645953a33824c3e31aa0547a87404
DESKTOP-FK4LHFN\prixi <prixia@naver.com>
2022-10-21 20:07:22 +0900

    백준 - 단계별로 풀어보기(문제집) : 2. 조건문(나머지)

---
+++ src/BaekJoon/Conditional_Mood/Problem14681.java
@@ -0,0 +1,27 @@
+package BaekJoon.Conditional_Mood;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem14681 {
+	public void solution() throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(bf.readLine());
+		int b = Integer.parseInt(bf.readLine());
+		
+		bf.close();
+		
+		if (a > 0 && b > 0) {
+			System.out.print("1");
+		} else if (a > 0 && b < 0) {
+			System.out.print("4");
+		} else if (a < 0 && b < 0) {
+			System.out.print("3");
+		} else if (a < 0 && b > 0) {
+			System.out.print("2");
+		}
+	}
+}
