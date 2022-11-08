a7c1e6919ba17c29ad60d6274e56c87213d319a5
DESKTOP-FK4LHFN\prixi <prixia@naver.com>
2022-11-08 13:30:23 +0900

    백준 - 단계별로 풀어보기(문제집) : 9. 기본 수학 2(6)

---
+++ src/BaekJoon/BasicMath_2/Problem9020.java
@@ -0,0 +1,63 @@
+package BaekJoon.BasicMath_2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class Problem9020 {
+	private int[] primeNumArr = new int[10000];
+	
+	public void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int caseCnt = Integer.parseInt(br.readLine());
+		List<Integer> numList = new ArrayList<>();
+		
+		for (int i = 0; i < caseCnt; i++) {
+			numList.add(Integer.parseInt(br.readLine()));
+		}
+
+		br.close();
+		
+		int maxNum = numList.stream().mapToInt(n -> n).max().getAsInt();
+		primeNumArr[0] = 1;
+		
+		for (int i = 1; i <= maxNum; i++) {
+			if (primeNumArr[i - 1] == 0) {
+				for (int j = 2; j * i <= 10000; j++) {
+					primeNumArr[j * i - 1] = 1;
+				}
+			}
+		}
+		
+		List<Integer> primeNumList = new ArrayList<>();
+		for (int i = 0; i < maxNum; i++) {
+			if (primeNumArr[i] == 0) {
+				primeNumList.add(i + 1);
+			}
+		}
+		
+		for (int value : numList) {
+			int aVal = 0;
+			int bVal = 0;
+			for (int i = 0, limit = value / 2; primeNumList.get(i) <= limit; i++) {
+				int tmp = value - primeNumList.get(i);
+				if (primeNumList.contains(tmp)) {
+					aVal = primeNumList.get(i);
+					bVal = tmp;
+				}
+			}
+			bw.write(aVal + " " + bVal + "\n");
+		}
+
+		bw.close();
+	}
+}
