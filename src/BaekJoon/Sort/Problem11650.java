d706793c99ae0e5d080e553b1dc51393946c2757
DESKTOP-FK4LHFN\prixi <prixia@naver.com>
2022-11-11 14:48:35 +0900

    백준 - 단계별로 풀어보기(문제집) : 10. 정렬(8)

---
+++ src/BaekJoon/Sort/Problem11650.java
@@ -0,0 +1,50 @@
+package BaekJoon.Sort;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Problem11650 {
+	public void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int inputCnt = Integer.parseInt(br.readLine());
+		List<Coordinate> valList = new ArrayList<>();
+		
+		for (int i = 0; i < inputCnt; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			valList.add(new Coordinate(st.nextToken(), st.nextToken()));
+		}
+		
+		valList.sort((a, b) -> a.getX() - b.getX() == 0 ? a.getY() - b.getY() : a.getX() - b.getX());
+		
+		for (Coordinate val : valList) {
+			bw.write(val.getX() + " " + val.getY() + "\n");
+		}
+		
+		br.close();
+		bw.close();
+	}
+	
+	class Coordinate {
+		private int x;
+		private int y;
+		
+		public Coordinate(String xStr, String yStr) {
+			this.x = Integer.parseInt(xStr);
+			this.y = Integer.parseInt(yStr);
+		}
+		public int getX() {
+			return x;
+		}
+		public int getY() {
+			return y;
+		}
+	}
+}
