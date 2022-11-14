ff871a49324f42d6e48a260902bb2dd332fa344f
DESKTOP-FK4LHFN\prixi <prixia@naver.com>
2022-11-14 16:06:27 +0900

    백준 - 단계별로 풀어보기(문제집) : 11. 재귀(2-3)

---
+++ src/BaekJoon/recursive/Problem10870.java
@@ -0,0 +1,37 @@
+package BaekJoon.recursive;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem10870 {
+	public void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int input = Integer.parseInt(br.readLine());
+		
+		bw.write(Integer.toString(fibonacci(input)));
+
+		br.close();
+		bw.close();
+	}
+	
+	private int fibonacci(int value) {
+		if (value == 0) {
+			return 0;
+		}
+		
+		if (value == 1) {
+			return 1;
+		}
+		
+		int result = 0;
+		
+		result += fibonacci(value - 2) + fibonacci(value - 1);
+		
+		return result;
+	}
+}
