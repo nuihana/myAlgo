a91f247df8fa66e33c8a260447b192747edfe250
DESKTOP-FK4LHFN\prixi <prixia@naver.com>
2022-11-23 14:03:01 +0900

    백준 - 단계별로 풀어보기(문제집) : 13. 집합과 맵(1-3)

---
+++ src/BaekJoon/SetAndMap/Problem14425.java
@@ -0,0 +1,37 @@
+package BaekJoon.SetAndMap;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Problem14425 {
+	public void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int possession = Integer.parseInt(st.nextToken());
+		int input = Integer.parseInt(st.nextToken());
+		int result = 0;
+		
+		Set<String> posSet = new HashSet<>();
+		for (int i = 0; i < possession; i++) {
+			posSet.add(br.readLine());
+		}
+		for (int i = 0; i < input; i++) {
+			if (posSet.contains(br.readLine())) {
+				result++;
+			}
+		}
+		
+		bw.write(Integer.toString(result));
+
+		bw.close();
+		br.close();
+	}
+}
