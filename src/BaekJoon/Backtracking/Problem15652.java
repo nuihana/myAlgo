f9fcff2dd41d944cdceb1194d480419e40e2a0b1
DUJEONG\prixi <dujeong@gaeasoft.co.kr>
2022-12-16 09:21:35 +0900

    백준 - 단계별로 풀어보기(문제집) : 16. 백트래킹(3-4)

---
+++ src/BaekJoon/Backtracking/Problem15652.java
@@ -0,0 +1,49 @@
+package BaekJoon.Backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem15652 {
+    private int[] arrs;
+    private StringBuilder sb = new StringBuilder();
+
+    public void solution() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        bf.close();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        arrs = new int[m];
+
+        printNum(n, m, 1, 0);
+
+        System.out.print(sb.toString());
+    }
+
+    private void printNum(int maxNum, int size, int pivot, int depth) {
+        /*
+         * 출력
+         */
+        if (size == depth) {
+            for (int num : arrs) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        /*
+         * 재귀
+         */
+        for (int i = pivot; i <= maxNum; i++) {
+            arrs[depth] = i;
+            printNum(maxNum, size, i, depth + 1);
+        }
+    }
+}
