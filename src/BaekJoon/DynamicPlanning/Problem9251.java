d8916b316e02bf6327f7536271bbc9a959c21041
DUJEONG\prixi <dujeong@gaeasoft.co.kr>
2023-01-16 08:09:23 +0900

    백준 - 단계별로 풀어보기(문제집) : 17. 동적 계획법 1(15)

---
+++ src/BaekJoon/DynamicPlanning/Problem9251.java
@@ -0,0 +1,44 @@
+package BaekJoon.DynamicPlanning;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Problem9251 {
+    char[] str1;
+    char[] str2;
+    Integer[][] dp;
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        str1 = br.readLine().toCharArray();
+        str2 = br.readLine().toCharArray();
+
+        dp = new Integer[str1.length][str2.length];
+
+        System.out.println(LCS(str1.length - 1, str2.length - 1));
+
+        br.close();
+    }
+
+    private int LCS(int x, int y) {
+        if (x == -1 || y == -1) {
+            return 0;
+        }
+
+        if (dp[x][y] == null) {
+            dp[x][y] = 0;
+
+            if (str1[x] == str2[y]) {
+                dp[x][y] = LCS(x - 1, y - 1) + 1;
+            } else {
+                dp[x][y] = Math.max(LCS(x - 1, y), LCS(x, y - 1));
+            }
+        }
+
+        return dp[x][y];
+    }
+}
