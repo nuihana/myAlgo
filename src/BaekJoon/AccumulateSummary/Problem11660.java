5a0f1b184627eee9b52514350d9cb7caf4ab7041
DUJEONG\prixi <dujeong@gaeasoft.co.kr>
2023-01-26 09:17:51 +0900

    백준 - 단계별로 풀어보기(문제집) : 18. 누적 합(5)

---
+++ src/BaekJoon/AccumulateSummary/Problem11660.java
@@ -0,0 +1,62 @@
+package BaekJoon.AccumulateSummary;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Problem11660 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int size = Integer.parseInt(st.nextToken());
+        int cnt = Integer.parseInt(st.nextToken());
+
+        int[][] sumArr = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < size; j++) {
+                sumArr[i][j] += Integer.parseInt(st.nextToken());
+                if (i > 0) {
+                    sumArr[i][j] += sumArr[i - 1][j];
+                }
+                if (j > 0) {
+                    sumArr[i][j] += sumArr[i][j - 1];
+                }
+                if (i > 0 && j > 0) {
+                    sumArr[i][j] -= sumArr[i - 1][j - 1];
+                }
+            }
+        }
+
+        for (int i = 0; i < cnt; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int bf_x = Integer.parseInt(st.nextToken());
+            int bf_y = Integer.parseInt(st.nextToken());
+
+            int af_x = Integer.parseInt(st.nextToken());
+            int af_y = Integer.parseInt(st.nextToken());
+
+            int result = sumArr[af_x - 1][af_y - 1];
+
+            if (bf_x > 1) {
+                result -= sumArr[bf_x - 2][af_y - 1];
+            }
+            if (bf_y > 1) {
+                result -= sumArr[af_x - 1][bf_y - 2];
+            }
+            if (bf_x > 1 && bf_y > 1) {
+                result += sumArr[bf_x - 2][bf_y - 2];
+            }
+
+            bw.write(result + "\n");
+        }
+
+        br.close();
+        bw.close();
+    }
+}
