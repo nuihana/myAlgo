09598657de121b86ccf51c62a08b79ced5c031d7
DUJEONG\prixi <dujeong@gaeasoft.co.kr>
2023-02-20 17:33:07 +0900

    백준 - 단계별로 풀어보기(문제집) : 23. 이분 탐색(3)

---
+++ src/BaekJoon/BinarySearch/Problem1654.java
@@ -0,0 +1,49 @@
+package BaekJoon.BinarySearch;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Problem1654 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int lanCnt = Integer.parseInt(st.nextToken());
+        int needCnt = Integer.parseInt(st.nextToken());
+        int[] lanArr = new int[lanCnt];
+
+        int max = 0;
+        for (int i = 0; i < lanCnt; i++) {
+            lanArr[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, lanArr[i]);
+        }
+
+        bw.write(Long.toString(getMaximumLength(lanArr, 0, max, needCnt)));
+
+        br.close();
+        bw.close();
+    }
+
+    private long getMaximumLength(int[] arr, long start, long end, int req) {
+        if (start > end) {
+            return 0;
+        }
+
+        long mid = (start + end) / 2;
+        mid = mid == 0 ? 1 : mid;
+
+        int computedLanCnt = 0;
+        for (int i = 0; i < arr.length; i++) {
+            computedLanCnt += arr[i] / mid;
+        }
+
+        if (computedLanCnt < req) {
+            return getMaximumLength(arr, start, mid - 1, req);
+        } else {
+            return Math.max(mid, getMaximumLength(arr, mid + 1, end, req));
+        }
+    }
+}
