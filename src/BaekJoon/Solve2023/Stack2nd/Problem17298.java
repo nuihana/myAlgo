29360686b56a3b6886deee3cce10e246f8a048a0
Daun Jeong <prixia@naver.com>
2023-03-11 22:43:04 +0900

    백준 - 단계별로 풀어보기(문제집) : 스택 2 일부

---
+++ src/BaekJoon/Solve2023/Stack2nd/Problem17298.java
@@ -0,0 +1,44 @@
+package BaekJoon.Solve2023.Stack2nd;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Problem17298 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arrSize = Integer.parseInt(br.readLine());
+        int[] arr = new int[arrSize];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arrSize; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[arrSize];
+        for (int i = arrSize - 1, j = 0; i >= 0; i--) {
+            while(!stack.empty() && stack.peek() < arr[i]) {
+                stack.pop();
+            }
+
+            if (stack.empty()) {
+                result[j] = -1;
+            } else {
+                result[j] = stack.peek();
+            }
+
+			stack.add(arr[i]);
+            j++;
+        }
+
+        for (int i = arrSize - 1; i >= 0; i--) {
+            bw.write(result[i] + " ");
+        }
+
+        br.close();
+        bw.close();
+    }
+}
