006ebfd876fe420215fe53809495bd2786491693
DESKTOP-FK4LHFN\prixi <prixia@naver.com>
2023-10-21 11:27:26 +0900

    백준 - 문자열 1문제

--- src/BaekJoon/Solve2023/October/String/Problem9248.java
+++ src/BaekJoon/Solve2023/October/String/Problem9248.java
@@ -1,44 +1,48 @@
 package BaekJoon.Solve2023.October.String;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
-public class Problem3033 {
+public class Problem9248 {
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int l = Integer.parseInt(br.readLine());
         String src = br.readLine();
+        List<Integer> suffixArray = makeSuffixArray(src);
+        int[] lcp = getLCP(src, suffixArray);
 
-        List<Integer> sa = getSuffixArray(src);
-        int[] lcp = getLCP(src, sa);
-        int res = 0;
-        for (int i = 0; i < lcp.length; i++) {
-            res = Math.max(res, lcp[i]);
+        for (int suf : suffixArray) {
+            bw.write((suf + 1) + " ");
+        }
+        bw.newLine();
+        bw.write("x ");
+        for (int cp : lcp) {
+            bw.write(cp + " ");
         }
 
-        System.out.print(res);
-
         br.close();
+        bw.close();
     }
 
-    private List<Integer> getSuffixArray(String src) {
+    private List<Integer> makeSuffixArray(String src) {
         int n = src.length();
         int t = 1;
 
         List<Integer> list = new ArrayList<>();
         int[] group = new int[n + 1];
+
         for (int i = 0; i < n; i++) {
             list.add(i);
             group[i] = src.charAt(i) - 'a';
         }
         group[n] = -1;
 
-        JduComparator cpr = new JduComparator(n, t, group);
+        JduComparator jcomp = new JduComparator(n, t, group);
         while (t < n) {
-            list.sort(cpr.comparator);
+            list.sort(jcomp.comp);
             t <<= 1;
             if (t >= n) break;
 
@@ -47,26 +51,26 @@ public class Problem3033 {
             newGroup[n] = -1;
 
             for (int i = 1; i < n; i++) {
-                if (cpr.comparator.compare(list.get(i - 1), list.get(i)) < 0) {
+                if (jcomp.comp.compare(list.get(i - 1), list.get(i)) < 0) {
                     newGroup[list.get(i)] = newGroup[list.get(i - 1)] + 1;
                 } else {
                     newGroup[list.get(i)] = newGroup[list.get(i - 1)];
                 }
             }
-            group = newGroup;
-            cpr.changeValues(t, group);
+
+            jcomp.setNewValue(t, newGroup);
         }
 
         return list;
     }
 
-    private int[] getLCP(String src, List<Integer> sa) {
-        int n = sa.size();
-        int[] lcp = new int[n - 1];
+    private int[] getLCP(String src, List<Integer> suffixArray) {
+        int n = suffixArray.size();
 
-        int[] isa = new int[n]; // inverse of sa
+        int[] isa = new int[n];
+        int[] lcp = new int[n - 1];
         for (int i = 0; i < n; i++) {
-            isa[sa.get(i)] = i;
+            isa[suffixArray.get(i)] = i;
         }
 
         int k = 0;
@@ -74,7 +78,7 @@ public class Problem3033 {
             int idx = isa[i];
             if (idx == n - 1) continue;
 
-            int j = sa.get(idx + 1);
+            int j = suffixArray.get(idx + 1);
             while (i + k < n && j + k < n) {
                 if (src.charAt(i + k) != src.charAt(j + k)) break;
                 k++;
@@ -88,15 +92,15 @@ public class Problem3033 {
     }
 
     class JduComparator {
-        private int n;
-        private int t;
-        private int[] group;
+        int n;
+        int t;
+        int[] group;
 
-        private Comparator<Integer> comparator = new Comparator<Integer>() {
+        Comparator<Integer> comp = new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
                 if (group[o1] != group[o2]) return group[o1] - group[o2];
-                int left = o1 + t, right = o2+ t;
+                int left = o1 + t, right = o2 + t;
                 if (left > n) left = n;
                 if (right > n) right = n;
                 return group[left] - group[right];
@@ -106,10 +110,10 @@ public class Problem3033 {
         public JduComparator(int n, int t, int[] group) {
             this.n = n;
             this.t = t;
-            this.group = Arrays.copyOf(group, group.length);
+            this.group = group;
         }
 
-        public void changeValues(int t, int[] group) {
+        public void setNewValue(int t, int[] group) {
             this.t = t;
             this.group = group;
         }
