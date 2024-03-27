0420932003af85195f33213d8dfbd67e185a44bb
정다운/지어소프트 <dujeong@gaeasoft.co.kr>
2024-03-27 12:48:52 +0900

    개별 - 스위스 라운드 매치 알고리즘

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,43 @@
-
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		br.close();
-		
-		int climb = Integer.parseInt(st.nextToken());
-		int fall = Integer.parseInt(st.nextToken());
-		int height = Integer.parseInt(st.nextToken());
-		
-		st = null;
-		
-		bw.write(Integer.toString((int)Math.ceil((double)(height - climb) / (climb - fall)) + 1));
-		bw.flush();
-		
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        List<char[]> list = new ArrayList<>();
+        while (true) {
+            String src = br.readLine();
+            if (src.length() < 2) break;
+
+            char[] mem = new char[]{ src.charAt(0), src.charAt(1) };
+            list.add(mem);
+        }
+
+        boolean[] isUsed = new boolean[list.size()];
+        while (isAllFilled(isUsed)) {
+            int a = (int) Math.floor(Math.random() * list.size());
+            while (isUsed[a]) a = (int) Math.floor(Math.random() * list.size());
+            isUsed[a] = true;
+            System.out.print("" + list.get(a)[0] + list.get(a)[1] + " vs ");
+
+            if (!isAllFilled(isUsed)) break;
+
+            int b = (int) Math.floor(Math.random() * list.size());
+            while (isUsed[b] || list.get(a)[0] == list.get(b)[0]) {
+                b = (int) Math.floor(Math.random() * list.size());
+            }
+            isUsed[b] = true;
+            System.out.println("" + list.get(b)[0] + list.get(b)[1]);
+        }
+
+        br.close();
+    }
+
+    private static boolean isAllFilled(boolean[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (!arr[i]) return true;
+        }
+        return false;
+    }
 }
