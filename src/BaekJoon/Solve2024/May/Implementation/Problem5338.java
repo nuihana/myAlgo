8a24d4c6fe87e148f7fb05a53f5ddc5f4da6e425
Daun Jeong <prixia@naver.com>
2024-05-04 09:51:52 +0900

    백준 - 구현 1문제

---
+++ src/BaekJoon/Solve2024/May/Implementation/Problem5338.java
@@ -0,0 +1,19 @@
+package BaekJoon.Solve2024.May.Implementation;
+
+public class Problem5338 {
+    public void solution() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("       _.-;;-._").append("\n");
+        sb.append("'-..-'|   ||   |").append("\n");
+        sb.append("'-..-'|_.-;;-._|").append("\n");
+        sb.append("'-..-'|   ||   |").append("\n");
+        sb.append("'-..-'|_.-''-._|");
+
+        System.out.print(sb);
+    }
+}
+
+
+
+
