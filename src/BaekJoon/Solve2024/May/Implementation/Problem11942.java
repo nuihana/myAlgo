9510bf81c27e32d56d9667bfacfb6e148d0fca5f
DESKTOP-FK4LHFN\prixi <prixia@naver.com>
2024-05-11 12:08:47 +0900

    백준 - 구현 1문제

---
+++ src/BaekJoon/Solve2024/May/Implementation/Problem11942.java
@@ -0,0 +1,11 @@
+package BaekJoon.Solve2024.May.Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem11942 {
+    public void solution() throws IOException {
+        System.out.print("고려대학교");
+    }
+}
