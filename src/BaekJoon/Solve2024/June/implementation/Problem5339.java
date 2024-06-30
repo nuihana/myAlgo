bbd2b8c0fb4cf31b3b2795290fa962f302d21ce6
DESKTOP-FK4LHFN\prixi <prixia@naver.com>
2024-06-30 13:39:37 +0900

    백준 - 구현 1문제

---
+++ src/BaekJoon/Solve2024/June/implementation/Problem5339.java
@@ -0,0 +1,19 @@
+package BaekJoon.Solve2024.June.Implementation;
+
+public class Problem5339 {
+    public void solution() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("     /~\\").append("\n");
+        sb.append("    ( oo|").append("\n");
+        sb.append("    _\\=/_").append("\n");
+        sb.append("   /  _  \\").append("\n");
+        sb.append("  //|/.\\|\\\\").append("\n");
+        sb.append(" ||  \\ /  ||").append("\n");
+        sb.append("============").append("\n");
+        sb.append("|          |").append("\n");
+        sb.append("|          |").append("\n");
+        sb.append("|          |");
+
+        System.out.print(sb);
+    }
+}
