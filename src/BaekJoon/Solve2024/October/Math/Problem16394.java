000f1474f673683c3803a3a334f2bd1c0f583d96
DESKTOP-FK4LHFN\prixi <prixia@naver.com>
2024-10-26 10:00:40 +0900

    백준 - 수학 1문제

---
+++ src/BaekJoon/Solve2024/October/Math/Problem16394.java
@@ -0,0 +1,16 @@
+package BaekJoon.Solve2024.October.Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem16394 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.print(n - 1946);
+
+        br.close();
+    }
+}
