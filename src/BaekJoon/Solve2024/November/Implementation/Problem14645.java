a283335a8f885f71d7098cbfd4c8ccc00a4a82d6
Daun Jeong <prixia@naver.com>
2024-11-03 23:32:17 +0900

    백준 - 구현 1문제

---
+++ src/BaekJoon/Solve2024/November/Implementation/Problem14645.java
@@ -0,0 +1,9 @@
+package BaekJoon.Solve2024.November.Implementation;
+
+import java.io.BufferedReader;
+
+public class Problem14645 {
+    public void solution() {
+        System.out.print("비와이");
+    }
+}
