2daa9d4f296ce474ba779c1f9b97fee40d9b3955
정다운/집 <prixia@naver.com>
2024-12-01 13:06:17 +0900

    백준 - 정렬 1문제

---
+++ src/BaekJoon/Solve2024/December/Sort/Problem2693.java
@@ -0,0 +1,31 @@
+package BaekJoon.Solve2024.December.Sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem2693 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int[] arr = new int[10];
+            for (int i = 0; i < 10; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(arr);
+
+            sb.append(arr[2]).append("\n");
+        }
+        System.out.print(sb);
+
+        br.close();
+    }
+}
