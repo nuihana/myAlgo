9b25f8e3b0d96172dece3a164b6429e4b34bf25e
정다운/집 <prixia@naver.com>
2024-12-28 11:19:21 +0900

    백준 - 구현 1문제

---
+++ src/BaekJoon/Solve2024/December/Implementation/Problem15740.java
@@ -0,0 +1,21 @@
+package BaekJoon.Solve2024.December.Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Problem15740 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+
+        System.out.print(a.add(b).toString());
+
+        br.close();
+    }
+}
