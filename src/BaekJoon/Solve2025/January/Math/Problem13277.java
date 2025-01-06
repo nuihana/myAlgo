667331394fc3d9dd59df589b3f289ae73a79165c
정다운/지어소프트 <dujeong@gaeasoft.co.kr>
2025-01-06 09:02:40 +0900

    백준 - 수학 1문제

---
+++ src/BaekJoon/Solve2025/January/Math/Problem13277.java
@@ -0,0 +1,21 @@
+package BaekJoon.Solve2025.January.Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Problem13277 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+
+        System.out.print(a.multiply(b));
+
+        br.close();
+    }
+}
