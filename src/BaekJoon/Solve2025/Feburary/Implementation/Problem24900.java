9c75ae0b0f98c3cceb13c6c211994ffafed448cf
정다운/지어소프트 <dujeong@gaeasoft.co.kr>
2025-02-12 09:15:38 +0900

    백준 - 구현 1문제

---
+++ src/BaekJoon/Solve2025/Feburary/Implementation/Problem24900.java
@@ -0,0 +1,74 @@
+package BaekJoon.Solve2025.Feburary.Implementation;
+
+public class Problem24900 {
+    public void solution() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("                                                           :8DDDDDDDDDDDDDD$.                                           ").append("\n");
+        sb.append("                                                      DDDNNN8~~~~~~~~~~=~7DNNDNDDDNNI                                   ").append("\n");
+        sb.append("                                                  ?NNDD=~=~~~~~~~~~~~~~~~~~=~~==~=INNDNN7                               ").append("\n");
+        sb.append("                                               +NDDI~~~~~~~~~~~~~~~~~~~~~~~=~~========~ODND+                            ").append("\n");
+        sb.append("                                            :NND~~~~~~~~~~~~~~~~~~~~~~~~~~~=~~============7NDN                          ").append("\n");
+        sb.append("                                          $DD$~~~~~~~~~~~~~~~~~~~~~~~~~~~~~=~~==============~DNN                        ").append("\n");
+        sb.append("                                        $DD=~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~=~~=================NND                      ").append("\n");
+        sb.append("                                       ND7~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~=~~===================DD7                    ").append("\n");
+        sb.append("                                     ~DD=~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~=======================8DN.                  ").append("\n");
+        sb.append("                                    8DO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~=========================DD                  ").append("\n");
+        sb.append("                                   8N~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~=~~=======================DN                 ").append("\n");
+        sb.append("                                  NN::::::::~~~~~~~~~~~=~~~~~~~~~~~~~~~~~~~=~~========================DDO               ").append("\n");
+        sb.append("                                 $D$:::::::::::::::~~~~DD~~~~~~~~~~~~~~~~~~=~~=========================DN.              ").append("\n");
+        sb.append("                                 D8:::::::::::::::::::DN=::~~~~~~~~~~~~~~~~=~~======================~~:~DN              ").append("\n");
+        sb.append("                                DN:::::::::::::::::::ONO::::::::::::::::::::~~~~~~~~~~~~:::::::::::::::::DN             ").append("\n");
+        sb.append("                               DN::::::::::::::::::::NN.:::::::::::::::::::::::::::DN::::::::::::::::::::$DO            ").append("\n");
+        sb.append("                               DD:::::::::::::::::::DNI:::::::::::::::::::::::::::::D=::::::::::::::::::::NN            ").append("\n");
+        sb.append("                              NN~~~~:::::$N?:::::::.NN::::::::::::::::::::::::::::::ND.:::::::::::::::::::+N8           ").append("\n");
+        sb.append("                              N7~~~~~~~~OD7::::::::~DD::::::::::::::::::::::::::::::~D$::::::::::::::::::::DN           ").append("\n");
+        sb.append("                             NN~~~~~~~~IDZ~~~~~::::DN~:::::::::::::::::::::::::::::::DN::::::::::::::::::::=N~          ").append("\n");
+        sb.append("                             DD~~~~~~~~NN~~~~~~~~~=NN::::::::::::::::::::::::::::::::DN:::::::::::::::~~====NN          ").append("\n");
+        sb.append("                            8D~~~~~~~~ND~~~~~~~~~~~ND~~~~~~~~:::::::::::::::::::::::::N7:::~~===============NN          ").append("\n");
+        sb.append("                            DD~~~~~~~ON+~~~~~~~~~~~ND~~~~~~~~~~~~~~~~~~~=+NZ==========NN====================~ND         ").append("\n");
+        sb.append("               :DD7   DNDD. N8~~~~~~~NN~~~~~~~~~~DDND~~~~~~~~~~~~~~~~~~~~ND~~=========DD=====================ND         ").append("\n");
+        sb.append("               N~:DDNNN .8NDN~~~~~~~$D=~~~~~~~~+ND.DD~~~~~~~~~~~~~~~~~~~=DD~~=========~D+====================DN         ").append("\n");
+        sb.append("              :D     .  ..~ND~~~~~~~NN~~~~~~~+NN$..ND~~~~~~~~~~~~~~~~~~~7N=~~=========~ND=======~============ON         ").append("\n");
+        sb.append("              NN       ...:N?~~~~~~~N=~~~~~NNNI.. .7D+~~~~~~~~~~~~~~~~~=8NN~~==========NN=======N============$N         ").append("\n");
+        sb.append("         N  ODN       ....DN~~~~~~~DD=8NNND$..     .DD~~~=~~~~~~~~~~~~~=NNDD=~=========8D~======NN===========~N$        ").append("\n");
+        sb.append("    N? =NN  ND      .....NND~~~~~~~DDNN:...        .ND=~DNN~~~~~~~~~~~~=DN.DN~=========?N+======NN============ND        ").append("\n");
+        sb.append("   $D? DN    DZ    ....ND8NN~~~~~~$D                .DD~NNDD~~~~~~~~~~~~D8..DN=========~DN======NN============DN        ").append("\n");
+        sb.append("   DN ~N~   NN    ..:~NN..NZ~~~~~~DN                  NNN8.ND~~~~NDN?~~~DZ...7DD=======~NN======NN============DN        ").append("\n");
+        sb.append("   ND DD    :DN.  ..ND$  .N?~~~~~=NNN                   . ..DDD$~N8OND8=N+   ..DDDZ~====NN======+D+===========ND        ").append("\n");
+        sb.append("   NO         DD  ZDN    8NO~~~~~~NNN..DDDNN7               ...NND...:DDD:     .:.NDND=~DD======~DO===========DN        ").append("\n");
+        sb.append("              DNDDN:.    DN~~~~~~=NNNN.ODNNNNDDNNO              ...     .         ...DNNNN=======ND===========DD        ").append("\n");
+        sb.append("       INDN7    DD.     .DD~~~~~=IDND:.:~.....?DNDNN.                        ...... ....$D=======ND===========ND        ").append("\n");
+        sb.append("       NN        ND.    8N=~~~~$ND::.:=~:.~=......=ND~                 .NNNNNNNNNNNNNNN.~N+======NN===========DN        ").append("\n");
+        sb.append("       $DD        DN:   DD~~~~7NO...~==.:~~:.....                      NNNND? ..::..7NZ.:N?======8D~==========ZN        ").append("\n");
+        sb.append("       DN?     ~D: DND.?D~~~~~DD....~:.~=~.......                            ....~=:.:~..ND======~N$==========~DO       ").append("\n");
+        sb.append("       ND    ..DD.  .DNDN=~~~~DI.......:.........                           ....=~..~~~..DN======~DD===========NN       ").append("\n");
+        sb.append("       DDD  :.:DD.  . DDI~~~~~ND................        .DNNNNNNNNNN7      ....=~:.:~~...NN=======ND===========?D~      ").append("\n");
+        sb.append("       8D. ...OD..    DD~~~~~~+ND ............          NN:~::::~~~8N      ........~~...:ND=======DN============NN      ").append("\n");
+        sb.append("       DDI:...ND     .D7~~~~~~~7NN ..........           ID8::::::::8D      .............:DN=======ON============NN      ").append("\n");
+        sb.append("        ~NNND.N=.   .NN~~~~~~~~~NDN8                       ~::::::~N8       .............DN========D=============NI     ").append("\n");
+        sb.append("               DDNNN.ND~~~~~~~~DD =DND                                       ............DN========N+~===========NN     ").append("\n");
+        sb.append("                   ~:N=~~~~~~~~DD   .DDDD                                       ........ NN========DD============8D     ").append("\n");
+        sb.append("                    8N~~~~~~~~~ND    . .7NDDD? .                                      .8DDN========NN=============D:    ").append("\n");
+        sb.append("                    DD~~~~~~~~~DND:         IDNNND$.                           .+DNNNNDNIDN========DD=============DD    ").append("\n");
+        sb.append("                    ND~~~~~~~~ZN 7DD .. .:DDNDDNNDNNNNDDNDND8$?===+$8DDNNNDDDDDN8I~DN====8N========NN=============NN    ").append("\n");
+        sb.append("                    DD~~~~~~~~8N   DD.  .NN~~~~.~~=DNDNO.:7ODDDDNNDD8DDDND=~~~ =~~~ON====8N========DN=============DN    ").append("\n");
+        sb.append("                    ND~~~~~~~~DN    ZDD  DN~~~ ~~~~~=.7DDD+.......8NNN==~~~~~ ~~~~~ONN$==DN========8N=============ON    ").append("\n");
+        sb.append("                    ND~8N~=~~~ZN      DDODN=~.~~~~~=.~~~~INDNNNNDNN~~~~~~~~:~~~~~~~DN~ND=DN========DD=========~ND=8N    ").append("\n");
+        sb.append("                    IN=NDDI~~~~D8       DNN::~~~~~.~~~~~=.~~ND~~ND~~~~~~~~.~~~~~~~~NN  NDNN====ND==ND~D?======DNN=ND    ").append("\n");
+        sb.append("                     DNNI8ND=~~DN:       ZN=~~~~~ ~~~~~.~~~~DD~=DD~~~~~~~ ~~~~~~~=.ND. . ND===DNDD=NDDNN=====8NZDDDN    ").append("\n");
+        sb.append("                      NND  IDNDNNN+       D+~~~:~~~~~~ ~~~~~DDNNN+~~~~~~~~~~~~~~:=?N7   .ND=~ND  DNNN~ID====ND7 NNN     ").append("\n");
+        sb.append("                       ID                 ND~~ ~~~~~:.~~~7DDN7IDNN==~~ ~~~~~~~~ ~~DN   .:N?DDDDD NND  8N~=DDD  ZNN      ").append("\n");
+        sb.append("                                          NN~:~~~~~ =7DDDD+8N  :N8DDZ.~~~~~~~~.~~~DD.   NDD+ . DN=     OND+             ").append("\n");
+        sb.append("                                          DND~~~=8DNDDZ=~~ ND   NN~INND~~~~~.~~~~ND .    .    ..IDD                     ").append("\n");
+        sb.append("                                         DDNNNDNNN+~~~~~~.7N.    ND~~~NDDI~ ~~~~=NNN             .DDI                   ").append("\n");
+        sb.append("                                        DN=~~~~.=~~~~~~ ~~DN     +N+~~~~+DNDD~~~NNNND.            ..ND                  ").append("\n");
+        sb.append("                                         DDI~~ ~~~~~~~ ~~~ND..  ..ND~~~~:~~~DNDNNNN+            ..7O8ND+                ").append("\n");
+        sb.append("                                          .DND=~~~~=::~~=NN.   . . 8D~~.~~~~~~=DN$ODNDNDNNNDNNNNND8+~..                 ").append("\n");
+        sb.append("                                             8DNNI=.~~~~=NDDNNNNDDNDNN.~~~~~IDDNDND7:.                                  ").append("\n");
+        sb.append("                                                ?DNNDD?~DD          ~NN~~=NDD$                                          ").append("\n");
+        sb.append("                                                     :DDD.            NNNN=                                             ");
+
+        System.out.print(sb);
+    }
+}
