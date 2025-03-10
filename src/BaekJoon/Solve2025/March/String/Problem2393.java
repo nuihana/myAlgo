9b10edf0e2b395731beee5015c609b7bc12c120b
정다운/지어소프트 <dujeong@gaeasoft.co.kr>
2025-03-10 09:06:47 +0900

    백준 - 문자열 1문제

---
+++ src/BaekJoon/Solve2025/March/String/Problem2393.java
@@ -0,0 +1,27 @@
+package BaekJoon.Solve2025.March.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem2393 {
+    public void solution() throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("  ___  ___  ___").append("\n");
+        sb.append("  | |__| |__| |").append("\n");
+        sb.append("  |           |").append("\n");
+        sb.append("   \\_________/").append("\n");
+        sb.append("    \\_______/").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |_____|").append("\n");
+        sb.append("  __/       \\__").append("\n");
+        sb.append(" /             \\").append("\n");
+        sb.append("/_______________\\");
+
+        System.out.print(sb);
+    }
+}
