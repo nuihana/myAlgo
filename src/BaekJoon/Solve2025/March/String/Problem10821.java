2b37111f726d723d34e40ec4cdb003ed8ce4f668
정다운/집 <prixia@naver.com>
2025-03-22 09:57:05 +0900

    백준 - 문자열 1문제

---
+++ src/BaekJoon/Solve2025/March/String/Problem10821.java
@@ -0,0 +1,15 @@
+package BaekJoon.Solve2025.March.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem10821 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print(br.readLine().split(",").length);
+
+        br.close();
+    }
+}
