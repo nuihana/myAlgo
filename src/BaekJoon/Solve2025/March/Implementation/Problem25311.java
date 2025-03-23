b979dea8a7109d3a5c048a1875c5bf8ab468036e
정다운/집 <prixia@naver.com>
2025-03-23 09:45:58 +0900

    백준 - 구현 1문제

---
+++ src/BaekJoon/Solve2025/March/Implementation/Problem25311.java
@@ -0,0 +1,15 @@
+package BaekJoon.Solve2025.March.Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem25311 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("A");
+
+        br.close();
+    }
+}
