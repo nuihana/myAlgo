06ecf86cfb9a349129326883b8cfeeae33add5eb
정다운/지어소프트 <dujeong@gaeasoft.co.kr>
2025-03-26 08:53:37 +0900

    백준 - 문자열 1문제

---
+++ src/BaekJoon/Solve2025/March/String/Problem10189.java
@@ -0,0 +1,16 @@
+package BaekJoon.Solve2025.March.String;
+
+import java.io.BufferedReader;
+
+public class Problem10189 {
+    public void solution() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("#  # #### #### #  #").append("\n");
+        sb.append("#### #  # #  # # #").append("\n");
+        sb.append("#### #  # #  # # #").append("\n");
+        sb.append("#  # #### #### #  #").append("\n");
+
+        System.out.print(sb);
+    }
+}
