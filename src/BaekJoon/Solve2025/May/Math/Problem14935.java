7a056d14e3901b75fad16b856d89217028e79206
정다운/집 <prixia@naver.com>
2025-05-24 11:04:27 +0900

    백준 - 수학 1문제

---
+++ src/BaekJoon/Solve2025/May/Math/Problem14935.java
@@ -0,0 +1,16 @@
+package BaekJoon.Solve2025.May.Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem14935 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String src = br.readLine();
+        System.out.print("FA");
+
+        br.close();
+    }
+}
