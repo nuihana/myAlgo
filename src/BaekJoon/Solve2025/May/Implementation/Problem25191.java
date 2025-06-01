06dee07fb0e3db4145290c59205461c96ee7237a
정다운/집 <prixia@naver.com>
2025-06-01 13:07:48 +0900

    백준 - 구현 1문제

---
+++ src/BaekJoon/Solve2025/May/Implementation/Problem25191.java
@@ -0,0 +1,21 @@
+package BaekJoon.Solve2025.May.Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem25191 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.print(Math.min(n, (a / 2 + b)));
+
+        br.close();
+    }
+}
