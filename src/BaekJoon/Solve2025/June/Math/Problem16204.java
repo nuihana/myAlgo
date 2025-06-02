7440cd23f85626b9fa713f6ca93582cf24e8ce60
정다운/집 <prixia@naver.com>
2025-06-03 07:06:12 +0900

    백준 - 수학 1문제

---
+++ src/BaekJoon/Solve2025/June/Math/Problem16204.java
@@ -0,0 +1,21 @@
+package BaekJoon.Solve2025.June.Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem16204 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        System.out.print(Math.min(m, k) + Math.min(n - m, n - k));
+
+        br.close();
+    }
+}
