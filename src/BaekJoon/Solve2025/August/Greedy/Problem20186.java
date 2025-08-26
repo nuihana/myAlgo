1a79e9a63f9d513f9aee21a4f22d411838127653
정다운/지어소프트 <dujeong@gaeasoft.co.kr>
2025-08-26 09:05:02 +0900

    백준 - 그리디 1문제

---
+++ src/BaekJoon/Solve2025/August/Greedy/Problem20186.java
@@ -0,0 +1,34 @@
+package BaekJoon.Solve2025.August.Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Problem20186 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Integer[] arr = new Integer[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        int res = 0;
+        for (int i = 0; i < k; i++) {
+            res += (arr[i] - i);
+        }
+        System.out.print(res);
+
+        br.close();
+    }
+}
