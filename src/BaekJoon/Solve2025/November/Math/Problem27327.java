076ac7f8f9d8af62094a802e61b562586d21dca2
정다운/지어소프트 <dujeong@gaeasoft.co.kr>
2025-11-25 09:16:51 +0900

    백준 - 수학 1문제

---
+++ src/BaekJoon/Solve2025/November/Math/Problem27327.java
@@ -0,0 +1,16 @@
+package BaekJoon.Solve2025.November.Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem27327 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine());
+        System.out.print(x * 24);
+
+        br.close();
+    }
+}
